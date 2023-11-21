4b31ebd1ca483094b43969c8edef83071f3fd08d
Monish <monishsharma080@gmail.com>
2023-11-22 00:20:31 +0530

    completed bg changer project

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { PaperProvider, useTheme, Text, Button, Provider, DefaultTheme, DarkTheme  } from 'react-native-paper';
+import { SafeAreaView, View, StyleSheet, Alert } from 'react-native';
 
-export default function App() {
+export default function Main() {
+
+  const theme = useTheme();
+  const [darkTheme, setdarkTheme ] = useState(theme.dark)// bool
+  
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <PaperProvider>
+        <View style={[styles.container,]} className={darkTheme ? " bg-slate-900  ": "bg-green-400"  } >
+        <Button mode="elevated" className={darkTheme ? "": "bg-green-800" }  onPress={() =>{
+          setdarkTheme(darkTheme ? false : true) ;
+        }}>
+            <Text style={darkTheme ? {} : {color:"#FFFFFF"} } >change theme from {darkTheme} </Text>
+          </Button>
+        </View>
+      </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  }
+})
